3da25a3f6b6a40d608ddb0e30f1396452d3ad2d4
AlgoRythm10 <142137701+AlgoRythm10@users.noreply.github.com>
2025-01-20 03:36:00 +0000

    baiscs and fundamentals

---
+++ 1_basics/string.js
@@ -0,0 +1,6 @@
+const name="adi"
+const repocount=50
+
+//console.log(name+repocount) this is a outdated method
+console.log(`hello my name is ${name} and my repo count is ${repocount}`);
+//this is called string interpolation
---
+++ 1_basics/string_conversion.js
@@ -0,0 +1,27 @@
+let someNUMBER=33
+
+let stringNumber=String(someNUMBER)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+//operations
+let value=3
+let negValue=-value
+//console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2%2)
+
+let str1="hello"
+let str2="adi"
+let str3=str1+str2
+console.log(str3);
+
+//confusing part-which data type comes firts is implemented and converted into
+// console.log("1"+2)//ouput 12
+// console.log(1+"2")//output 12
+// console.log("1"+2+2)//output 122
+// console.log(1+2+"2")//output 32
